4e01c7a67701fa651dc905b5d1ae6ad8a87fc263
Allan Penoni <rochapenoni@gmail.com>
2025-02-16 20:11:42 -0300

    Adicionar arquivos de projeto.

---
+++ NumeroAleatorio/Program.cs
@@ -0,0 +1,24 @@
+﻿Random numeroAleatorio = new Random();
+int numeroSecreto = numeroAleatorio.Next(1, 101);
+Console.WriteLine(numeroSecreto);
+do
+{
+    Console.WriteLine("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número secreto.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número secreto é maior.");
+    }
+    else
+    {
+        Console.WriteLine("O número secreto é menor.");
+    }
+} while (true);
+
+Console.WriteLine("Você acertou o número secreto! Parabéns!");
